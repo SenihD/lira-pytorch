986c32f6bd2ca1fb3678187a6e26541e273bc2c8
Osman Demirci <osmandemirci1999@gmail.com>
2026-08-02 12:49:57 +0300

    N_SHADOWS fix

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@
 # Set this to "malimg" or "cifar10" to toggle the pipeline
 
 DATASET="malimg"
-EPOCHS=3
-N_SHADOWS=1
+EPOCHS=1
+N_SHADOWS=4
 
 echo "========================================"
 echo "Starting LiRA Pipeline for: $DATASET"
@@ -18,10 +18,9 @@ echo "[1/4] Training Shadow Models..."
 for ((id=0; id<N_SHADOWS; id++))
 do
     echo " -> Training Shadow Model $id"
-    python3 train.py --dataset $DATASET --epochs $EPOCHS --shadow_id $id
+    python3 train.py --dataset $DATASET --epochs $EPOCHS --shadow_id $id --n_shadows $N_SHADOWS
 done
 
-'''
 # 2. Run Inference
 echo "[2/4] Running Inference..."
 python3 inference.py --dataset $DATASET
@@ -33,4 +32,6 @@ python3 score.py --dataset $DATASET
 # 4. Generate FPR/TPR Plots
 echo "[4/4] Generating Plots..."
 python3 plot.py --dataset $DATASET
-'''
+
+
+
